f797eb4a94d1fab15931f32f588ddebdc38abaa2
ssalguei <sergio.salgueiro-monforte@capgemini.com>
2024-10-08 16:23:42 +0200

    prueba

--- CapSteam/src/main/java/com/bootcamp/CapSteam/CapSteamApplication.java
+++ CapSteam/src/main/java/com/bootcamp/CapSteam/CapSteamApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapSteamApplication {
-
+	//prueba
 	public static void main(String[] args) {
 		SpringApplication.run(CapSteamApplication.class, args);
 	}
