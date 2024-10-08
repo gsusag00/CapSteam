66eeae27982f66701a9ed1e9b7a79e8090d397be
gsusag00 <gsus.ag00@proton.me>
2024-10-08 16:04:53 +0200

    Crear proyecto

---
+++ CapSteam/src/main/java/com/bootcamp/CapSteam/CapSteamApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.CapSteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapSteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapSteamApplication.class, args);
+	}
+
+}
---
+++ CapSteam/src/test/java/com/bootcamp/CapSteam/CapSteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.CapSteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapSteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
